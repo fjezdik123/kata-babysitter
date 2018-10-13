3ce6ec975cfc84fae6ae74e193310150b4e3248e
Frank Jezdik <frankjezdik@gmail.com>
2018-10-13 13:46:23 -0400

    ErrorControllerTest

---
+++ PayoutCalcApp.Tests/Controllers/ErrorControllerTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PayoutCalcApp.Controllers;
+using PayoutCalcApp.Models;
+
+namespace PayoutCalcApp.Tests.Controllers
+{
+    [TestClass]
+    public class ErrorControllerTest
+    {
+        [TestMethod]
+        public void PageErrorFound_GetRedirectResult_ErrorIsNull()
+        {
+           
+            var controller = new ErrorController();
+
+            // Act
+            var result = controller.PageErrorFound(null) as RedirectResult;
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("~/Payout",result.Url);
+          
+        }
+
+        [TestMethod]
+        public void PageErrorFound_GetRedirectResult_ErrorIsEmpty()
+        {
+
+            var controller = new ErrorController();
+
+            // Act
+            var result = controller.PageErrorFound(string.Empty) as RedirectResult;
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("~/Payout", result.Url);
+
+        }
+
+        [TestMethod]
+        public void PageErrorFound_GetView_ErrorIsNotNullOrEmpty()
+        {
+
+            var controller = new ErrorController();
+            var msg = "Error";
+            // Act
+            var result = controller.PageErrorFound(msg) as ViewResult;
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(msg,result.Model.ToString());
+
+        }
+
+    }
+}
